183c2e06fb493f94572dcdd973463ffea24ff282
Adam Bagley <adam.bagley@tssecurities.com>
2026-03-18 08:43:04 -0400

    add license header to sprintf

--- sw/lib/src/sprintf.c
+++ sw/lib/src/sprintf.c
@@ -1,4 +1,20 @@
 /*
+ *    Copyright 2026 Two Sigma Open Source, LLC
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+/*
  * sprintf.c  –  portable sprintf / snprintf, no <stdio.h> dependency.
  *
  * Floating-point uses integer-scaling (multiply |d|×10^prec, round in
